ff467be289411b3d9d9c50ce09cb78606403720f
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2023-04-07 19:36:59 +0300

    Implemented model description view

--- app/src/main/java/com/main/VehiclesModelDetailsFragment.kt
+++ app/src/main/java/com/main/VehiclesModelDetailsFragment.kt
@@ -1,11 +1,14 @@
 package com.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
@@ -14,16 +17,17 @@ import com.example.androidproject.R
 import com.main.sevices.ModelService
 import org.koin.android.ext.android.inject
 
-/**
- * A simple [Fragment] subclass.
- * Use the [VehiclesModelDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class VehiclesModelDetailsFragment : Fragment() {
 
     private lateinit var _backButton: Button
+    private lateinit var _shareButton: ImageButton
     private lateinit var _navController: NavController
     private lateinit var _titleTextView: TextView
+    private lateinit var _bodyTypeTextView: TextView
+    private lateinit var _startYearTextView: TextView
+    private lateinit var _seatNrTextView: TextView
+    private lateinit var _descriptionTextView: TextView
+    private lateinit var _modelImage: ImageView
 
     private val _modelService: ModelService by inject()
 
@@ -40,20 +44,75 @@ class VehiclesModelDetailsFragment : Fragment() {
 
         _navController = this.findNavController()
         _backButton = view.findViewById<View>(R.id.modelDetailsBackButton) as Button
+        _shareButton = view.findViewById<View>(R.id.shareButton) as ImageButton
 
         _backButton.setOnClickListener{
             _navController.navigate(R.id.action_vehicleModelDetailsFragment_to_ModelsFragment)
         }
 
+        //get selected model using the service
         val model = _modelService.getSelectedModel()
 
-        _titleTextView =view.findViewById<View>(R.id.modelDetailsTitle) as TextView
+        _shareButton.setOnClickListener{
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+
+            val content =  buildString {
+                append(model.brandName)
+                append(" ")
+                append(model.modelName)
+                append("\n")
+                append(model.bodyType)
+                append("\n")
+                append(model.nrOfSeats)
+                append("\n")
+                append(model.startYear)
+                append("\n")
+                append(model.description)
+            }
+
+            shareIntent.putExtra(Intent.EXTRA_TEXT, content)
+            startActivity(Intent.createChooser(shareIntent, "Share"))
+        }
+
+        _titleTextView = view.findViewById<View>(R.id.modelDetailsTitle) as TextView
+        _bodyTypeTextView = view.findViewById<View>(R.id.modelDetailsBodyType) as TextView
+        _startYearTextView = view.findViewById<View>(R.id.modelDetailsStartYear) as TextView
+        _seatNrTextView = view.findViewById<View>(R.id.modelDetailsSeatNr) as TextView
+        _descriptionTextView = view.findViewById<View>(R.id.modelDetailsDescription) as TextView
+        _modelImage = view.findViewById<View>(R.id.modelImage) as ImageView
+
+        _modelImage.setImageBitmap(model.image)
+
         _titleTextView.text = buildString {
             append(model.brandName)
             append(" ")
             append(model.modelName)
         }
-        
+
+        _bodyTypeTextView.text = buildString {
+            append(getString(R.string.body_type_string))
+            append(": ")
+            append(model.bodyType)
+        }
+
+        _startYearTextView.text = buildString {
+            append(getString(R.string.start_year_string))
+            append(": ")
+            append(model.startYear)
+        }
+
+        _seatNrTextView.text = buildString {
+            append(getString(R.string.seat_number_string))
+            append(": ")
+            append(model.nrOfSeats)
+        }
+
+        _descriptionTextView.text = buildString {
+            append(getString(R.string.description_string))
+            append(": ")
+            append(model.description)
+        }
     }
 
     override fun onResume() {
